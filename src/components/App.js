10f71a35edb6c405bed1c470d8c80d8eb0320e74
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-04-04 12:37:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,75 @@
-import React, { useState } from "react";
-import UseMemo from "./UseMemo";
-import ReactMemo from "./ReactMemo";
+import React, { useState, useMemo } from "react";
+import UseMemoComponent from "./components/UseMemoComponent";
+import ReactMemoComponent from "./components/ReactMemoComponent";
+import "./styles.css";
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [newTodo, setNewTodo] = useState("");
 
-  const onAdd = () => {
-    setTasks((prev) => [...prev, "New Todo"]);
+  // Function to add a default "New todo"
+  const addTodo = () => {
+    setTodos([...todos, "New todo"]);
   };
+
+  // Function to add a validated custom todo
+  const addCustomTodo = () => {
+    if (newTodo.length > 5) {
+      setTodos([...todos, newTodo]);
+      setNewTodo(""); // Clear input after adding
+    } else {
+      alert("Todo must be more than 5 characters!");
+    }
+  };
+
+  // Memoized count operation
+  const computedValue = useMemo(() => {
+    console.log("Computing expensive operation...");
+    return counter * 10;
+  }, [counter]);
+
   return (
-    <div id="main">
-      <h1>React.useMemo</h1>
-      <h2>My todos</h2>
-      <ul>
-        {tasks.map((i, index) => (
-          <li key={index}>{i}</li>
-        ))}
-      </ul>
+    <div className="app-container">
+      <h1>React Memo & useMemo Example</h1>
 
-      <button onClick={onAdd}>Add Todo</button>
+      {/* Counter Section */}
+      <div>
+        <h2>Counter: {counter}</h2>
+        <button id="increment-btn" onClick={() => setCounter(counter + 1)}>
+          Increment Counter
+        </button>
+      </div>
 
-      <hr />
-      <UseMemo />
-      <hr />
-      <hr />
-      <ReactMemo />
+      {/* Computed Value */}
+      <h3>Computed Value: {computedValue}</h3>
+
+      {/* Todo List Section */}
+      <div>
+        <h2>Todo List</h2>
+        <button id="add-todo-btn" onClick={addTodo}>Add Todo</button>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Add Custom Todo */}
+      <div>
+        <input
+          id="todo-input"
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter a todo (min 6 chars)"
+        />
+        <button id="submit-btn" onClick={addCustomTodo}>Submit</button>
+      </div>
+
+      {/* Memo Components */}
+      <UseMemoComponent counter={counter} />
+      <ReactMemoComponent todos={todos} />
     </div>
   );
 };
